58f94b203c39fde2a1731376ea89a0ca4a47036b
ASBcep <asaboritb2324@politecnics.barcelona>
2024-01-08 20:28:06 +0100

    Converit a recyclerview. peta.

--- app/src/main/java/asb/m08/m08_uf1_23_videojocs/MainActivity.kt
+++ app/src/main/java/asb/m08/m08_uf1_23_videojocs/MainActivity.kt
@@ -1,63 +1,56 @@
 package asb.m08.m08_uf1_23_videojocs
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.ListView
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-/*
-* Has de crear un nou projecte Android anomenat Videojocs.
-* Has de configurar la teva aplicació per què es vegi en horitzontal,
-* encara que mantinguem el dispositiu en vertical (investigueu com).
-* En la part esquerra de la pantalla, es mostrarà una llista de
-* videojocs (han d’haver-hi entre 8 i 10). A la llista,
-* per cada videojoc es mostrarà una imatge i el seu nom.
-* Quan l’usuari faci clic sobre un videojoc de la llista,
-* es mostrarà en la part dreta de la pantalla la seva imatge
-* (en gran) i el seu nom (sota la imatge gran) i un número
-* corresponent al número de jugadors que han jugat
-* (us ho podeu inventar). Aquesta última dada no es mostrarà a la
-* llista; només es mostrarà a la part dreta. Serà tasca teva decidir
-* quins jocs formaran part de la llista i trobar les imatges
-* (que hauran de ser rectangulars i no quadrades).
-* */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         setContentView(R.layout.activity_main)
 
-        var llistaMutableVideojocs = mutableListOf(Videojoc("Red Dead Redemption II", R.drawable.rdr2, 150000000),
-                                            Videojoc("Grand Theft Auto V", R.drawable.gtav, 1000000000),
-                                            Videojoc("Kingdom Hearts", R.drawable.kingdomhearts, 52424774),
-                                            Videojoc("Super Mario Odissey", R.drawable.smo, 245789544),
-                                            Videojoc("Pokémon edición Amarilla", R.drawable.pkmnamarillo, 999999999),
-                                            Videojoc("Spyro 3: Year of the Dragon", R.drawable.spyro3, 65484825),
-                                            Videojoc("Crash Bandicoot 2", R.drawable.crash2, 547471415),
-                                            Videojoc("Metroid Dread", R.drawable.metroidread, 3389985),
-                                            Videojoc("Hogwarts Legacy", R.drawable.hogwartslegacy, 514789),
-                                            Videojoc("International Rally Championship", R.drawable.irc, 130000),
-                                            )
-        val lstVideojocs = this.findViewById(R.id.LstVwListVj) as ListView
+        val videojocs = getVideojocs()
 
-        val adapter = VideojocAdapter(this, R.layout.listview_videojocs_items, llistaMutableVideojocs)
+        val lstVideojocs = findViewById(R.id.RclrVwListVj) as RecyclerView
+
+        val adapter = VideojocAdapter(this, videojocs)
+        lstVideojocs.hasFixedSize()
+        lstVideojocs.layoutManager = LinearLayoutManager(this)
         lstVideojocs.adapter = adapter
 
-        lstVideojocs.setOnItemClickListener()
+        adapter.setOnClickListener()
         {
-            adapterView, view, i, l ->
-
+            val videojoc = videojocs[lstVideojocs.getChildAdapterPosition(it)]
             val lblVideojocTitol = findViewById(R.id.TxtVwTitol) as TextView
-            lblVideojocTitol.text = llistaMutableVideojocs[i].titol
+            lblVideojocTitol.text = videojoc.titol
 
             val imgVideojoc = findViewById(R.id.ImgVwCaratula) as ImageView
-            imgVideojoc.setImageResource(llistaMutableVideojocs[i].imatge)
+            imgVideojoc.setImageResource(videojoc.imatge)
 
             val lblVideojocJugadors = findViewById(R.id.TxtVwJugs) as TextView
-            lblVideojocJugadors.text = "Jugadors: " + llistaMutableVideojocs[i].jugadors.toString()
-
+            lblVideojocJugadors.text = "Jugadors: " + videojoc.jugadors
         }
+    }
+    fun getVideojocs(): MutableList<Videojoc>{
+        return mutableListOf(
+        Videojoc("Red Dead Redemption II", R.drawable.rdr2, 150000000),
+        Videojoc("Grand Theft Auto V", R.drawable.gtav, 1000000000),
+        Videojoc("Kingdom Hearts", R.drawable.kingdomhearts, 52424774),
+        Videojoc("Super Mario Odissey", R.drawable.smo, 245789544),
+        Videojoc("Pokémon edición Amarilla", R.drawable.pkmnamarillo, 999999999),
+        Videojoc("Spyro 3: Year of the Dragon", R.drawable.spyro3, 65484825),
+        Videojoc("Crash Bandicoot 2", R.drawable.crash2, 547471415),
+        Videojoc("Metroid Dread", R.drawable.metroidread, 3389985),
+        Videojoc("Hogwarts Legacy", R.drawable.hogwartslegacy, 514789),
+        Videojoc("International Rally Championship", R.drawable.irc, 130000),
+        )
     }
 }
--- app/src/main/java/asb/m08/m08_uf1_23_videojocs/VideojocAdapter.kt
+++ app/src/main/java/asb/m08/m08_uf1_23_videojocs/VideojocAdapter.kt
@@ -1,16 +1,58 @@
 package asb.m08.m08_uf1_23_videojocs
 
 import android.content.Context
+import android.graphics.BitmapFactory
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
-class VideojocAdapter (context: Context, val layout: Int, val videojocs: MutableList<Videojoc>):
-    ArrayAdapter<Videojoc>(context, layout, videojocs)
+class VideojocAdapter (private val context: Context, private val videojocs: MutableList<Videojoc>):
+    RecyclerView.Adapter<VideojocAdapter.VideojocViewHolder>(),
+    View.OnClickListener
 {
+    private val layout = R.layout.listview_videojocs_items
+    private var clickListener: View.OnClickListener? = null
+    class VideojocViewHolder(val view: View): RecyclerView.ViewHolder(view)
+    {
+        val videojocCaratula = view.findViewById(R.id.ImgVwCaratulaList) as ImageView
+        val videojocTitol = view.findViewById(R.id.TxtVwTitolList) as TextView
+        val videojocJugadors = view.findViewById(R.id.TxtVwJugsList) as TextView
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideojocViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(layout, parent, false)
+
+        view.setOnClickListener(this)
+
+        return VideojocViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: VideojocViewHolder, position: Int) {
+    val videojoc = videojocs[position]
+    bindVideojoc(holder, videojoc)
+    }
+    override fun getItemCount() = videojocs.size
+    fun bindVideojoc(holder: VideojocViewHolder, videojoc: Videojoc)
+    {
+        val videojocPath = context.getFilesDir().toString() + "/img/" + videojoc.imatge
+        val bitmap = BitmapFactory.decodeFile(videojocPath)
+        holder.videojocCaratula?.setImageBitmap(bitmap)
+        holder.videojocTitol?.text = videojoc.titol
+        holder.videojocJugadors?.text = "Jugadors: " + videojoc.jugadors
+    }
+    fun setOnClickListener(listener: View.OnClickListener)
+    {
+        clickListener = listener
+    }
+
+    override fun onClick(view: View?)
+    {
+        clickListener?.onClick(view)
+    }
+}
+    /*//original a partir d'aquí
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View
     {
         var view: View
@@ -37,5 +79,4 @@ class VideojocAdapter (context: Context, val layout: Int, val videojocs: Mutable
         val videojocJugadors = view.findViewById(R.id.TxtVwJugsList) as TextView
         videojocJugadors.text = "Jugadors: " + videojoc.jugadors
 
-    }
-}
+    }*/
